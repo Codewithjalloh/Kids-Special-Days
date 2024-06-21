4aecb7e2f589c9739aa47a4a8e6905e0812e0bbd
codewithjalloh <codewithjalloh@gmail.com>
2024-06-21 23:52:22 +0100

    Initial Commit

---
+++ Kids' Special Days/Kids__Special_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kids__Special_DaysApp.swift
+//  Kids' Special Days
+//
+//  Created by Lord Jalloh on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kids__Special_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
